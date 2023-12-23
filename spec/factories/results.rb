f0ccb756f2551af7c075aa6de01eaabb94724c4f
tomcharo <shota.a_2807@softbank.ne.jp>
2023-12-23 23:20:02 +0900

    resultモデル単体テスト

--- spec/factories/results.rb
+++ spec/factories/results.rb
@@ -1,5 +1,7 @@
 FactoryBot.define do
   factory :result do
-    
+    name {"testname"}
+    category_id {1}
+    association :user
   end
 end
--- spec/models/result_spec.rb
+++ spec/models/result_spec.rb
@@ -1,5 +1,32 @@
 require 'rails_helper'
 
 RSpec.describe Result, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @result = FactoryBot.build(:result)
+  end
+
+  describe "成績登録" do
+    context "成績登録できる場合" do
+      it "全て入力されていれば登録できる" do
+        expect(@result).to be_valid
+      end
+    end
+    context "成績登録できない場合" do
+      it "name:空" do
+        @result.name = ""
+        @result.valid?
+        expect(@result.errors.full_messages).to include("Name can't be blank")
+      end
+      it "category_id:0" do
+        @result.category_id = 0
+        @result.valid?
+        expect(@result.errors.full_messages).to include("Category is invalid")
+      end
+      it "user:紐づいていない" do
+        @result.user = nil
+        @result.valid?
+        expect(@result.errors.full_messages).to include("User must exist")
+      end
+    end
+  end
 end
