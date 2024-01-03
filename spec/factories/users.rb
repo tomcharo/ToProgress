808f0b9720fdc4da3422f1edb7b9a96e4b18e41f
tomcharo <shota.a_2807@softbank.ne.jp>
2024-01-03 14:04:23 +0900

    結合テストログイン処理

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -6,5 +6,9 @@ FactoryBot.define do
     last_name {"山田"}
     first_name {"トム"}
     grade_id {1}
+
+    factory :user_student do
+      grade_id {2}
+    end
   end
 end
---
+++ spec/support/sign_in_support.rb
@@ -0,0 +1,15 @@
+module SignInSupport
+  def sign_in(user)
+    name = ENV["BASIC_AUTH_USER"]
+    password = ENV["BASIC_AUTH_PASSWORD"]
+    visit "http://#{name}:#{password}@#{Capybara.current_session.server.host}:#{Capybara.current_session.server.port}#{root_path}"
+
+    visit new_user_session_path
+    fill_in "user_email", with: user.email
+    fill_in "user_password", with: user.password
+    find('input[name="commit"]').click
+    sleep 1
+    expect(page).to have_current_path(root_path)
+    expect(page).to have_content(user.grade.name)
+  end
+end
