8b4150a8396769a05329d52bc865896d73aebc86
tomcharo <shota.a_2807@softbank.ne.jp>
2024-01-08 23:15:39 +0900

    faker導入

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,14 +1,16 @@
 FactoryBot.define do
   factory :user do
-    email {"test@spectest"}
+    email {Faker::Internet.email}
     password {"111111"}
     password_confirmation {password}
-    last_name {"山田"}
-    first_name {"トム"}
-    grade_id {1}
+    last_name {"高橋"}
+    first_name {"葵"}
+    grade_id {2}
 
-    factory :user_student do
-      grade_id {2}
+    factory :user_teacher do
+      last_name {"山田"}
+      first_name {"トム"}
+      grade_id {1}
     end
   end
 end
