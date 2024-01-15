9f2db09dd20b0bffc68324d419ffe0d5db854eaf
tomcharo <shota.a_2807@softbank.ne.jp>
2024-01-15 23:23:55 +0900

    結合テスト科目登録

--- spec/factories/subjects.rb
+++ spec/factories/subjects.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
   factory :subject do
-    name {"testname"}
+    name {"spec科目"}
     score {50}
     max_score {100}
     average_score {40}
--- spec/system/subjects_spec.rb
+++ spec/system/subjects_spec.rb
@@ -1,9 +1,95 @@
 require 'rails_helper'
 
-RSpec.describe "Subjects", type: :system do
+RSpec.describe "科目登録", type: :system do
   before do
-    driven_by(:rack_test)
+    @subject = FactoryBot.build(:subject)
+    # 成績作成
+    result = FactoryBot.create(:result)
+    @subject.result = result
+    # 先生ユーザー作成
+    @teacher = FactoryBot.create(:user_teacher)
   end
 
-  pending "add some scenarios (or delete) #{__FILE__}"
+  context "科目登録できる場合" do
+    it "生徒が自身の成績の科目を登録できる" do
+      # 生徒ユーザーでログイン
+      sign_in(@subject.result.user)
+      # 成績一覧ページに遷移
+      click_on "成績一覧"
+      sleep 0.1
+      expect(current_path).to eq(student_results_path(@subject.result.user))
+      # 成績詳細ページに遷移
+      click_on @subject.result.name
+      sleep 0.1
+      expect(current_path).to eq(student_result_path(@subject.result.user, @subject.result))
+      # 科目登録ページに遷移
+      click_on "編集"
+      sleep 0.1
+      expect(current_path).to eq(new_student_result_subject_path(@subject.result.user, @subject.result))
+      # 値を入力して送信、Subjectモデルカウント確認
+      fill_in "subject_name", with: @subject.name
+      fill_in "subject_score", with: @subject.score
+      fill_in "subject_max_score", with: @subject.max_score
+      fill_in "subject_average_score", with: @subject.average_score
+      fill_in "subject_rank", with: @subject.rank
+      fill_in "subject_rank_range", with: @subject.rank_range
+      expect{
+        find('input[value="保存"]').click
+        sleep 0.1
+      }.to change{Subject.count}.by(1)
+      # 適切に表示されていることを確認(科目登録ページ)
+      expect(current_path).to eq(new_student_result_subject_path(@subject.result.user, @subject.result))
+      expect(page).to have_selector(".header", text: @subject.result.user.last_name)
+      expect(page).to have_selector(".main_right", text: @subject.result.name)
+      expect(page).to have_content(@subject.name)
+      # 成績詳細ページに遷移
+      click_on "終了"
+      sleep 0.1
+      expect(current_path).to eq(student_result_path(@subject.result.user, @subject.result))
+      expect(page).to have_content(@subject.name)
+    end
+
+    it "先生が生徒の成績の科目を登録できる" do
+      # 先生ユーザーでログイン
+      sign_in(@teacher)
+      # 生徒一覧ページに遷移
+      click_on "生徒一覧"
+      sleep 0.1
+      expect(current_path).to eq(students_path)
+      # 生徒の成績一覧ページに遷移
+      click_on @subject.result.user.last_name
+      sleep 0.1
+      expect(current_path).to eq(student_results_path(@subject.result.user))
+      # 成績詳細ページに遷移
+      click_on @subject.result.name
+      sleep 0.1
+      expect(current_path).to eq(student_result_path(@subject.result.user, @subject.result))
+      # 科目登録ページに遷移
+      click_on "編集"
+      sleep 0.1
+      expect(current_path).to eq(new_student_result_subject_path(@subject.result.user, @subject.result))
+      # 値を入力して送信、Subjectモデルカウント確認
+      fill_in "subject_name", with: @subject.name
+      fill_in "subject_score", with: @subject.score
+      fill_in "subject_max_score", with: @subject.max_score
+      fill_in "subject_average_score", with: @subject.average_score
+      fill_in "subject_rank", with: @subject.rank
+      fill_in "subject_rank_range", with: @subject.rank_range
+      expect{
+        find('input[value="保存"]').click
+        sleep 0.1
+      }.to change{Subject.count}.by(1)
+      # 適切に表示されていることを確認(科目登録ページ)
+      expect(current_path).to eq(new_student_result_subject_path(@subject.result.user, @subject.result))
+      expect(page).to have_selector(".header", text: @teacher.last_name)
+      expect(page).to have_selector(".main_right", text: @subject.result.user.last_name)
+      expect(page).to have_selector(".main_right", text: @subject.result.name)
+      expect(page).to have_content(@subject.name)
+      # 成績詳細ページに遷移
+      click_on "終了"
+      sleep 0.1
+      expect(current_path).to eq(student_result_path(@subject.result.user, @subject.result))
+      expect(page).to have_content(@subject.name)
+    end
+  end
 end
